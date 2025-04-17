663958b346d03e943ab1e7fddff71f83299ec83b
varshath04 <145836423+varshath04@users.noreply.github.com>
2025-04-17 10:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,6 @@ def delete_poll(id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
